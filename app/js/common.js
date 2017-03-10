36feb210c7f692e3943ec4a600c53a30bc3a4cfc
Tolala9 <ly.an.mode1@gmail.com>
2017-03-10 18:58:40 +0200

    adaptive header top line

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,20 @@
 $(function() {
 
+	$(".toggle-mnu").click(function() {
+		$(this).toggleClass("on");
+		$(this).parent().next().next().find(".main-mnu").slideToggle();
+		return false;
+	});
+
+	$(".main-foot .toggle-mnu").click(function() {
+		$("html, body").animate({scrollTop: $(document).height() + 200}, "slow");
+		return false;
+	});
+
+	$("body").on("click", ".top", function() {
+		$("html, body").animate({scrollTop: 0}, "slow");
+	});
+
 	//SVG Fallback
 	if(!Modernizr.svg) {
 		$("img[src*='svg']").attr("src", function() {
@@ -13,28 +28,60 @@ $(function() {
 		var th = $(this);
 		$.ajax({
 			type: "POST",
-			url: "mail.php", //Change
+			url: "/mail.php", //Change
 			data: th.serialize()
 		}).done(function() {
-			alert("Thank you!");
+			th.find(".success").addClass("active");
 			setTimeout(function() {
 				// Done Functions
+				th.find(".success").removeClass("active");
 				th.trigger("reset");
-			}, 1000);
+				$.magnificPopup.close();
+			}, 3000);
 		});
 		return false;
 	});
 
-	//Chrome Smooth Scroll
-	try {
-		$.browserSelector();
-		if($("html").hasClass("chrome")) {
-			$.smoothScroll();
-		}
-	} catch(err) {
-
-	};
+	$("body").append('<div class="top"><i class="fa fa-angle-double-up">');
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+	//Replace all SVG images with inline SVG
+	$('img.img-svg').each(function(){
+		var $img = $(this);
+		var imgClass = $img.attr('class');
+		var imgURL = $img.attr('src');
+
+		$.get(imgURL, function(data) {
+				// Get the SVG tag, ignore the rest
+				var $svg = $(data).find('svg');
+
+				// Add replaced image's classes to the new SVG
+				if(typeof imgClass !== 'undefined') {
+					$svg = $svg.attr('class', imgClass+' replaced-svg');
+				}
+
+				// Remove any invalid XML tags as per http://validator.w3.org
+				$svg = $svg.removeAttr('xmlns:a');
+
+				// Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+				if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+					$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+				}
+
+				// Replace image with new SVG
+				$img.replaceWith($svg);
+
+			}, 'xml');
+
+	});
+
+	$(window).scroll(function() {
+		if($(this).scrollTop() > $(this).height()) {
+			$(".top").addClass("active");
+		} else {
+			$(".top").removeClass("active");
+		}
+	});
 
 });
