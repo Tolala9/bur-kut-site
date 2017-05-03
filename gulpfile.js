a76b4ea1f482a1594edf0399b68d57bda7190a55
tolala9 <anatolii.lukavenko@gmail.com>
2017-05-03 10:38:29 +0300

    Make Footer (regular)

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,6 @@ gulp.task('scripts', function() {
 		'./app/libs/animateNumber/jquery.animateNumber.min.js',
 		'./app/libs/equalHeights/equalHeights.min.js',
 		'./app/libs/owl-carousel/owl.carousel.min.js',
-		'./app/libs/selectize/dist/js/standalone/selectize.min.js',
 		])
 		.pipe(concat('libs.js'))
 		.pipe(uglify()) //Minify libs.js
