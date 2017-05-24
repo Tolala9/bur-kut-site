fbdf591fedfce8ff791e08e18e7a1cded943c8fc
Tolala9 <anatolii.lukavenko@gmail.com>
2017-05-24 18:08:09 +0300

    Progect changed for deploy on hosting (new gulp file & changed links for fonts and img

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
-var gulp         = require('gulp'),
+var gulp         = require('gulp'), 
 		sass         = require('gulp-sass'),
 		autoprefixer = require('gulp-autoprefixer'),
 		cleanCSS     = require('gulp-clean-css'),
 		rename       = require('gulp-rename'),
 		browserSync  = require('browser-sync').create(),
 		concat       = require('gulp-concat'),
-		uglify       = require('gulp-uglify');
+		uglify       = require('gulp-uglify'),
+		gutil          = require('gulp-util' ),
+		del            = require('del'),
+		imagemin       = require('gulp-imagemin'),
+		cache          = require('gulp-cache'),
+		ftp            = require('vinyl-ftp'),
+		notify         = require("gulp-notify");
 
 gulp.task('browser-sync', ['styles', 'scripts'], function() {
 		browserSync.init({
@@ -52,3 +58,56 @@ gulp.task('watch', function () {
 });
 
 gulp.task('default', ['browser-sync', 'watch']);
+
+gulp.task('imagemin', function() {
+	return gulp.src('app/img/**/*')
+	.pipe(cache(imagemin()))
+	.pipe(gulp.dest('dist/img')); 
+});
+
+gulp.task('build', ['removedist', 'imagemin', 'styles', 'scripts'], function() {
+
+	var buildFiles = gulp.src([
+		'app/*.html',
+		'app/.htaccess',
+		]).pipe(gulp.dest('dist'));
+
+	var buildCss = gulp.src([
+		'app/css/main.min.css',
+		]).pipe(gulp.dest('dist/css'));
+
+	var buildJs = gulp.src([
+		'app/js/common.js',
+		]).pipe(gulp.dest('dist/js'));
+
+	var buildJs = gulp.src([
+		'app/js/libs.js',
+		]).pipe(gulp.dest('dist/js'));
+
+	var buildFonts = gulp.src([
+		'app/fonts/**/*',
+		]).pipe(gulp.dest('dist/fonts'));
+
+});
+
+gulp.task('deploy', function() {
+
+	var conn = ftp.create({
+		host:      'hostname.com',
+		user:      'username',
+		password:  'userpassword',
+		parallel:  10,
+		log: gutil.log
+	});
+
+	var globs = [
+	'dist/**',
+	'dist/.htaccess',
+	];
+	return gulp.src(globs, {buffer: false})
+	.pipe(conn.dest('/path/to/folder/on/server'));
+
+});
+
+gulp.task('removedist', function() { return del.sync('dist'); });
+gulp.task('clearcache', function () { return cache.clearAll(); });
